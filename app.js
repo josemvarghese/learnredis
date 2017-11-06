b2705816243c31a488d64d7d31b65ed55af604ca
jose <jose@voris.co.in>
2017-11-06 18:32:13 +0530

    initial templates

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const path = require('path');
 const PORT = 3000;
+const redis = require('redis');
 
 
 
@@ -10,15 +11,31 @@ const PORT = 3000;
 
 
 const app = express();
-
-
+const client = redis.createClient();
+client.on('error', function (err) {
+    console.log("Error " + err);
+});	
 app.use(express.static(path.join(__dirname, 'static')));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
 
-
+app.post('/api/addinfo',function(req, res, next) {
+		console.log(req.body.name);
+	console.log(req.body.desc);
+	res.json({name:req.body.name});
+});
 app.get('*', (req, res) => {
+
+  // const username='jose';
+  // client.get(username, function(error, result) {
+	 //  	if (result) {
+	 //        console.log(result)
+	 //    } 
+	 //    else {
+	 //      	client.setex(username, 60, 'jose');
+	 //    }
+  // });
   res.sendFile(path.join(__dirname, 'static/templates/index.html'));
 });
 
